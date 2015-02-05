3a53e6d319f50955e7292ce97a0c9431773f8d31
David Kudera <kudera.d@gmail.com>
2015-02-05 16:18:52 +0100

    tests: removed option to set custom config files

--- tests/CarrooiTests/Labels/TestCase.php
+++ tests/CarrooiTests/Labels/TestCase.php
@@ -15,27 +15,17 @@ class TestCase extends BaseTestCase
 
 
 	/**
-	 * @param string $customConfig
 	 * @return \Nette\DI\Container
 	 */
-	protected function createContainer($customConfig = null)
+	protected function createContainer()
 	{
 		copy(__DIR__. '/../LabelsApp/Model/database', TEMP_DIR. '/database');
 
 		$config = new Configurator;
 		$config->setTempDirectory(TEMP_DIR);
-		$config->addParameters(['container' => ['class' => 'SystemContainer_' . md5($customConfig)]]);
 		$config->addParameters(['appDir' => __DIR__. '/../LabelsApp']);
 		$config->addConfig(__DIR__. '/../LabelsApp/config/config.neon');
 		$config->addConfig(FileMock::create('parameters: {databasePath: %tempDir%/database}', 'neon'));
-
-		if ($customConfig) {
-			if (pathinfo($customConfig, PATHINFO_EXTENSION) !== 'neon') {
-				$customConfig = __DIR__. '/../LabelsApp/config/'. $customConfig. '.neon';
-			}
-
-			$config->addConfig($customConfig);
-		}
 
 		return $config->createContainer();
 	}
